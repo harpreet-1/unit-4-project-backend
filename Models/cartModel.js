c93eb8434ce5f403d58f6355238582d10ac68f34
harpreet-1 <hpsllg8@gmail.com>
2023-04-02 22:39:35 +0530

    done

--- Models/cartModel.js
+++ Models/cartModel.js
@@ -2,15 +2,18 @@ const mongoose = require("mongoose");
 const cartSchema = mongoose.Schema({
   title: { type: String, required: true },
   category: { type: String },
-  image1: { type: String, required: true },
-  discount: { type: String },
-  quantity: { type: Number, default: 1 },
-  userID: { type: String, required: true },
-  size: String,
-  description: String,
   price: Number,
 });
 
 const cartModel = mongoose.model("cart", cartSchema);
 
+let a = "harry";
+let b = "harry";
 module.exports = { cartModel };
+
+// image1: { type: String, required: true },
+// discount: { type: String },
+// quantity: { type: Number, default: 1 },
+// userID: { type: String, required: true },
+// size: String,
+// description: String,
