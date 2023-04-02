b2f08d13c4e58ad6a133fac3294e25480f4c57c1
harpreet-1 <hpsllg8@gmail.com>
2023-04-02 22:19:44 +0530

    done

--- Models/cartModel.js
+++ Models/cartModel.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 const cartSchema = mongoose.Schema({
   title: { type: String, required: true },
-  category: { type: String, required: true },
-  images: { type: Array, required: true },
-  discount: { type: Number, default: 0 },
+  category: { type: String },
+  image1: { type: String, required: true },
+  discount: { type: String },
   quantity: { type: Number, default: 1 },
   userID: { type: String, required: true },
+  size: String,
   description: String,
   price: Number,
 });
